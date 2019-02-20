90cbc66df28b356758b48c4b8195eff1037fa7c2
Stephen Wright <scw248@gmail.com>
2019-02-20 16:15:37 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,4 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
-  # def build_network(arg)
-  #   network = Network.new(arg)
-  #   network.show = self
-  # end
 end
